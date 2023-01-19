1724a6ae650ff97bfb1c4f415835e2f68de4a876
MaksymKolii <maksymkolii@gmail.com>
2023-01-19 17:45:12 +0100

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { contactsReducer } from './contacts/contactsSlice';
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './filter/filterSlice';
 import {authReducer } from './auth/authSlice'
+// import { persistStore } from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
