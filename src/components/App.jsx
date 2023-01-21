66b9484fb3700e80107a0e3e2da1548bdc2e889f
Maksym Kolii <maksymkolii@gmail.com>
2023-01-21 16:54:38 +0100

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { fetchCurrentUser } from '../redux/auth/auth-operations';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from 'Pages/HomePage/HomePage';
 import { RegisterPage } from 'Pages/RegisterPage/RegisterPage';
@@ -9,53 +9,62 @@ import { UsersPage } from 'Pages/UsersPage/UsersPage';
 import { Layout } from 'Pages/Layout/Layout';
 import { PrivateRoute } from 'HOCs/PrivateRoute';
 import { PublicRoute } from 'HOCs/PublicRoute';
+import { selectIsFetchingCurrentUser } from '../redux/auth/auth-selectors';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import { GlobalStyle } from '../components/Utils/GlobalStyle';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(selectIsFetchingCurrentUser);
 
   useEffect(() => {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <PublicRoute>
-                <HomePage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="users"
-            element={
-              <PrivateRoute>
-                <UsersPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="register"
-            element={
-              <PublicRoute restricted >
-                <RegisterPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="login"
-            element={
-              <PublicRoute restricted >
-                <LoginPage />
-              </PublicRoute>
-            }
-          />
-        </Route>
-      </Routes>
+      {isFetchingCurrentUser ? <p>Wait a little bit</p> : (
+        <ChakraProvider>
+          {' '}
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route
+                index
+                element={
+                  <PublicRoute>
+                    <HomePage />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="users"
+                element={
+                  <PrivateRoute>
+                    <UsersPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="register"
+                element={
+                  <PublicRoute restricted>
+                    <RegisterPage />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="login"
+                element={
+                  <PublicRoute restricted>
+                    <LoginPage />
+                  </PublicRoute>
+                }
+              />
+            </Route>
+          </Routes>
+        </ChakraProvider>
+      )}
+
       <GlobalStyle />
     </>
   );
--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store , persistor} from 'redux/store';
-import { ChakraProvider } from '@chakra-ui/react'
+
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider> 
       <Provider store={store}> 
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter  basename="/goit-react-hw-08-phonebook">
@@ -18,6 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </BrowserRouter>
       </PersistGate> 
     </Provider>
-    </ChakraProvider>
   </React.StrictMode>
 );
